a02172850563e2d3c1204c1755d8a4d9a238d72e
Italo souza <nos.100@hotmail.com>
2022-06-22 18:12:47 -0300

    italo

---
+++ src/data/Docentes.ts
@@ -0,0 +1,31 @@
+
+import { User } from "./User";
+import { Especialidades } from "../Modal/Especialidades";
+
+
+
+export class Docente extends User{
+
+    private especialidades: Especialidades[] = [];
+
+
+    constructor(
+        id: string,
+        nome: string,
+        email: string,
+        data_nasceu: string,
+        turma_id: string,
+        especiallidades: Especialidades[]
+    ){
+        super(id, nome, email, data_nasceu, turma_id);
+        console.log("Confirmando cadastro");
+        this.especialidades = especiallidades;
+    }
+
+
+    public getEspecialidades(): Especialidades[]{
+        return this.especialidades
+    }
+
+}
+
